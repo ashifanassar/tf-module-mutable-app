b7ab88327cd2b8f2236ef39c3c5556b05e9ee091
ashifanassar <ashifanassar@gmail.com>
2024-05-07 00:10:49 +0530

    modules

--- ec2.tf
+++ ec2.tf
@@ -3,7 +3,7 @@ resource "aws_spot_instance_request" "spot" {
   count                   = var.SPOT_INSTANCE_COUNT
   ami                       = data.aws_ami.ami.id
   instance_type             = var.SPOT_INSTANCE_TYPE
-  subnet_id                 = var.INTERNAL ? element(data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNET_IDS, count.index) : data.terraform_remote_state.vpc.outputs.PUBLIC_SUBNET_IDS
+  subnet_id                 = var.INTERNAL ? element(data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNET_IDS, count.index) : element(data.terraform_remote_state.vpc.outputs.PUBLIC_SUBNET_IDS, count.index)
   vpc_security_group_ids    = [aws_security_group.allow_app.id]
   wait_for_fulfillment      = true 
   iam_instance_profile      = "DevopsRole"
