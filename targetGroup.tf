fb8bacf08d6a39260fcad850c6cc2adbeb4b02e4
ashifanassar <ashifanassar@gmail.com>
2024-05-09 23:35:22 +0530

    module

--- targetGroup.tf
+++ targetGroup.tf
@@ -5,6 +5,17 @@ resource "aws_lb_target_group" "app" {
   port     = var.APP_PORT
   protocol = "HTTP"
   vpc_id   = data.terraform_remote_state.vpc.outputs.VPC_ID
+
+
+
+  health_check {
+    path                = "/health"         # Path to check for health (e.g., a health endpoint) 
+    healthy_threshold   = 2                 # Minimum number of successful checks before marking healthy
+    unhealthy_threshold = 2                 # Maximum number of failed checks before marking unhealthy
+    timeout             = 4                 # Timeout in seconds for each health check attempt
+    interval            = 5 
+    enabled             = true 
+  }
 }
 
 #Register the backend instance with the target group
