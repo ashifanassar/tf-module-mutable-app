005f98f7d5db9689514f841c97861fd299281e65
ashifanassar <ashifanassar@gmail.com>
2024-05-10 19:33:21 +0530

    module

--- app.tf
+++ app.tf
@@ -17,7 +17,7 @@ resource "null_resource" "app_deploy" {
 
   provisioner "remote-exec" {
     inline = [
-      "ansible-pull -U https://github.com/ashifanassar/ansible.git -e ENV=dev -e COMPONENT=${var.COMPONENT} -e DOCDB_ENDPOINT=${data.terraform_remote_state.db.outputs.DOCDB_ENDPOINT} -e REDIS_ENDPOINT=${data.terraform_remote_state.db.outputs.REDIS_ENDPOINT[0]} -e APP_VERSION=${var.APP_VERSION} roboshop-pull.yml"
+      "ansible-pull -U https://github.com/ashifanassar/ansible.git -e ENV=dev -e MYSQL_ENDPOINT=${data.terraform_remote_state.db.outputs.MYSQL_ENDPOINT} -e COMPONENT=${var.COMPONENT} -e DOCDB_ENDPOINT=${data.terraform_remote_state.db.outputs.DOCDB_ENDPOINT} -e REDIS_ENDPOINT=${data.terraform_remote_state.db.outputs.REDIS_ENDPOINT[0]} -e APP_VERSION=${var.APP_VERSION} roboshop-pull.yml"
     ]
   }
 }
